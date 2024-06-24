Added new functions and made corrections to the KagomeLattice class - Corrected calculation of number of unit cell rows in KagomeLattice class - generate_randomids() will now make n_attempts to populate the lattice with n_molecules before throwing an error - Added pairwise_distances() function to pairwise_distance.py - Added transition_rates() and ideal_boxwidth() functions to energy.py - Added plot_molecules, figsize_xscale(), and figsize_yscale() functions to plotter.py

src/energy.py
@@ -5,14 +5,62 @@ from math import sqrt, pi
 import numpy as np
 import scipy as sp
 
-from .constants import A0, HT, HV
+from .constants import A0, KB, HT, HV
 
 
 # Wrapper for Scipy's Struve function of order 0 
-scipy_H0 = lambda x: sp.special.struve(0, x)
+_scipy_H0 = lambda x: sp.special.struve(0, x)
 
 # Wrapper for Scipy's Neumann function of order 0 (a.k.a. Bessel function of the second kind of order 0)
-scipy_Y0 = lambda x: sp.special.y0(x)
+_scipy_Y0 = lambda x: sp.special.y0(x)
+
+def ideal_boxwidths(tl_lc=4, tl_range=[2, 78], kl_lc=0.246, kl_range=[32, 1254], show_topn=10):
+    """Computes the ideal box widths for the triangular and Kagome lattices
+
+    Parameters
+    ----------
+    tl_lc : float
+        The lattice constant of the triangular lattice (in nm)
+    tl_range : list
+        The range of box widths for the triangular lattice (in number of lattice constants)
+    kl_lc : float
+        The lattice constant of the Kagome lattice (in nm)
+    kl_range : list
+        The range of box widths for the Kagome lattice (in number of lattice constants)
+    show_topn : int
+        The number of top box widths to display
+
+    Returns
+    -------
+    None
+    
+    """
+    tle_x = tl_lc * np.arange(tl_range[0], tl_range[1])
+    tle_cols = np.array(tle_x//tl_lc, dtype=int)
+    tle_rows = np.array(tle_cols/np.sqrt(3), dtype=int)
+    tle_y = tl_lc * np.sqrt(3) * tle_rows
+    print(f"tle_x (first and last value): {tle_x[0]}, {tle_x[-1]}")
+    kl_x = kl_lc * np.arange(kl_range[0], kl_range[1])
+    kl_cols = np.array(kl_x//kl_lc, dtype=int)
+    kl_rows = np.array(np.round(kl_cols/np.sqrt(3)), dtype=int)
+    kl_y = kl_lc * np.sqrt(3) * kl_rows
+    print(f"\nkl_x (first and last value): {kl_x[0]}, {kl_x[-1]}\n")
+    min_ids = np.zeros(len(kl_x), dtype=int)
+    min_vals = np.zeros(len(kl_x))
+    min_diffs = np.zeros(len(kl_x))
+    print("Ideal box widths (sorted by L2-norm error):")
+    for i, (j, k) in enumerate(zip(kl_x, kl_y)):
+        xdiffs = np.abs(tle_x-j)**2
+        ydiffs = np.abs(tle_y-k)**2
+        diffs = np.sqrt(xdiffs + ydiffs)
+        min_ids[i] = np.argmin(diffs)
+        min_vals[i] = j
+        min_diffs[i] = diffs[min_ids[i]]
+    sort_ids = np.argsort(min_diffs)
+    min_vals = min_vals[sort_ids]
+    min_diffs = min_diffs[sort_ids]
+    for i in range(show_topn):
+        print(f"#{i+1}: {min_vals[i]:.3f}nm | L2-norm error: {min_diffs[i]:.4f}")
 
 def kappa_h(eps_r=4.22, mu=0.100, v_f=1.49e6):
     """Computes the inverse screening radius in Hartree atomic units
@@ -62,15 +110,56 @@ def total_impurity_energies(r, Zval=1.0, epsr=4.22):
     khr = kappa_h(eps_r=epsr) * r
     khr_cpu = khr.get()
     Va = cp.where(r!=0.0, 1./r, cp.zeros_like(r)) * (Zval / epsr)
-    Vb = cp.where(khr!=0.0, cp.array(scipy_H0(khr_cpu) - scipy_Y0(khr_cpu)), cp.zeros_like(khr))
+    Vb = cp.where(khr!=0.0, cp.array(_scipy_H0(khr_cpu) - _scipy_Y0(khr_cpu)), cp.zeros_like(khr))
     Vc = Va * (1.0 - (0.5 * pi * khr * Vb))
     # Reduce the dimensions of the array depending on dimensions of the input
     Vth = Vc if (r_ndims == 1) or (r_ndims is None) else cp.sum(Vc, axis=r_ndims-1)
     return Vth
 
+def transition_rates(m_energies, nn_energies, temperature):
+    """Computes the transition rates between the microstates of the system
+
+    Parameters
+    ----------
+    m_energies : array-like
+        The energies of the molecules in the system 
+        (in Hartree atomic units)
+    nn_energies : array-like
+        The energies of the nearest neighbors of the molecules in the system 
+        (in Hartree atomic units)
+    temperature : float
+        The temperature of the system 
+        (in Kelvin)
+
+    Returns
+    -------
+    rates : array-like
+        The transition rates between the molecules and their nearest neighbors sites
+        (in dimensionless units)
+    """
+    m_ndims = len(m_energies.shape)
+    nn_ndims = len(nn_energies.shape)
+    if m_ndims != (nn_ndims - 1):
+        raise ValueError('m_energies and nn_energies must be 1D and 2D arrays respectively.')
+    factor = HT / (2 * KB * temperature)
+    energy_differences = m_energies[:,None] - nn_energies
+    return cp.exp(factor * energy_differences)
 
 class TriangularLatticeEnergies: 
-    """Class to define the energy of a particle on a triangular lattice."""
+    """Class to define the energy of a particle on a triangular lattice.
+    
+    Parameters
+    ----------
+    lattice_constant : float
+        The lattice constant of the triangular lattice
+    amplitude : float
+        The amplitude of the potential energy
+
+    Methods
+    -------
+    U : array-like
+        Evaluates the potential energy at the given x, y coordinates
+    """
     def __init__(self, lattice_constant=4.0, amplitude=1.0):
         self.lattice_constant = lattice_constant
         self.amplitude = amplitude * 11.605  # In terms of meV converted to Kelvin
@@ -86,6 +175,6 @@ class TriangularLatticeEnergies:
         x2 = (x+shift[0])*cp.cos(angle) - (y+shift[1])*cp.sin(angle)
         y2 = (x+shift[0])*cp.sin(angle) + (y+shift[1])*cp.cos(angle)
         a, A = self.lattice_constant, self.amplitude
-        theta_x, theta_y = 2 * pi * x2 / a, 2 * pi * y2 / (a * sqrt(3))
+        theta_x, theta_y = 2 * pi * x2 / a, 2 * pi * y2 / (a * np.sqrt(3))
         sigma = 2 * cp.cos(theta_x) * cp.cos(theta_y) + cp.cos(2 * (theta_y))
         return (A * 2 / 9 * ((3 - sigma)))

src/plotter.py
@@ -2,8 +2,8 @@
 
 import cupy as cp
 from matplotlib.collections import LineCollection
-import matplotlib.pyplot as plt
 import numpy as np
+
 
 def _graphene_lines(row, col, sqrt3, lc, lbp, colors, lw, ls, alpha, zorder):
     """Return the bond lines of a graphene lattice unit cell as a LineCollection object."""
@@ -38,6 +38,12 @@ def _graphene_lines(row, col, sqrt3, lc, lbp, colors, lw, ls, alpha, zorder):
     ])
     return LineCollection(bonds + lbp, colors=colors, lw=lw, ls=ls, alpha=alpha, zorder=zorder)
 
+def figsize_xscale(L):
+    return (L._n_columns*L._lc) / (L._n_rows*L._lc*np.sqrt(3))
+
+def figsize_yscale(L):
+    return (L._n_rows*L._lc*np.sqrt(3)) / (L._n_columns*L._lc)
+
 def plot_graphenebonds(ax, L, colors='b', lw=0.5, ls='--', alpha=0.25, zorder=2):
     """Plot the graphene lattice lines on a given axis."""
     lbp = L._lbp.get()
@@ -48,14 +54,23 @@ def plot_graphenebonds(ax, L, colors='b', lw=0.5, ls='--', alpha=0.25, zorder=2)
 
 def plot_kagomesites(ax, L, s=35, c='k', ec='k', alpha=0.3, zorder=3):
     """Plot the Kagome lattice sites on a given axis."""
-    lxy = L.get_latticesites.get()
+    lxy = L.get_latticesites().get()
     ax.scatter(lxy[:,0], lxy[:,1], s=s, c=c, ec=ec, alpha=alpha, zorder=zorder)
     return ax
 
 def plot_latticeenergies(ax, LE, boxsize, angle=0., shift=[0., 0.], nsamples=1000, cmap='viridis', alpha=0.2, zorder=1):
+    """Plot the lattice energies on a given axis."""
     xtri = cp.linspace(0.0, boxsize[0], nsamples)
     ytri = cp.linspace(0.0, boxsize[1], nsamples)
     Xtri, Ytri = cp.meshgrid(xtri, ytri)
     Utri = LE.U(Xtri, Ytri, angle, shift)
     ax.contourf(Xtri.get(), Ytri.get(), Utri.get(), cmap='viridis', alpha=0.2, zorder=zorder)
+    return ax
+
+def plot_molecules(ax, mnnxy, show_nn=False, s=100, mc='tab:red', nnc='yellow', ec='k', lw=2.5, alpha=1.0, zorder=4):
+    """Plot the molecules on a given axis."""
+    # Highlight the site corresponding to the selected ID in red
+    ax.scatter(mnnxy[:,0,0], mnnxy[:,0,1], s=s, c=mc, ec=ec, lw=lw, alpha=alpha, zorder=zorder)
+    # Highlight the nearest-neighbors of selected ID in yellow
+    if show_nn: ax.scatter(mnnxy[:,1:,0], mnnxy[:,1:,1], s=s, c=nnc, ec=ec, lw=lw, alpha=alpha, zorder=zorder)
     return ax